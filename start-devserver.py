87294e793c04ab3108a540564d160a5562ab8cae
Geert Barentsen <geert@barentsen.be>
2016-03-24 11:55:21 -0700

    disable forking in devserver to enable the in-browser debugging to work

--- start-devserver.py
+++ start-devserver.py
@@ -7,4 +7,4 @@ from astroplanapp import app
 
 if __name__ == "__main__":
     app.debug = True
-    app.run(port=8080, host='0.0.0.0', processes=3)
+    app.run(port=8080, host='0.0.0.0', debug=True)
